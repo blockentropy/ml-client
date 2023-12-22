357219769b155170f2575f004ef5a45663e1107c
Edward Kim <edwardkim.ek@gmail.com>
2023-12-22 13:52:05 -0500

    added long lingua

--- compress_client.py
+++ compress_client.py
@@ -30,20 +30,25 @@ args = parser.parse_args()
 logging.basicConfig(level=logging.DEBUG)
 
 class CompletionRequest(BaseModel):
-    model: Optional[str] = "Yi"
-    input: Union[str, List[str], List[List[str]]]
-    output: Union[str, List[str], List[List[str]]]
+    model: Optional[str] = "Starling-LM-7B-alpha"
+    prompt: Union[str, List[str], List[List[str]]]
+    user: Optional[str] = None
+
+class CompletionRequestLong(BaseModel):
+    model: Optional[str] = "Starling-LM-7B-alpha"
+    prompt: Union[str, List[str], List[List[str]]]
+    question: Union[str, List[str], List[List[str]]]
     user: Optional[str] = None
 
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-#repo_id = config.get('PairRM', 'repo')
+repo_id = config.get('Starling-LM-7B-alpha', 'repo')
 host = config.get('settings', 'host')
 port = args.port if args.port is not None else config.getint('settings', 'port')
 
-llm_lingua = PromptCompressor()
+llm_lingua = PromptCompressor(repo_id, model_config={"revision": "main"})
 
 print("*** Loaded.. now Rank...:")
 
@@ -54,12 +59,11 @@ async def main(request: CompletionRequest):
 
     response_data = None
     try:
-        print(request.input)
-        print(request.output)
-
+        print(request.prompt)
         
-        compressed_prompt = llm_lingua.compress_prompt(prompt, instruction="", question="", target_token=200)
+        compressed_prompt = llm_lingua.compress_prompt(request.prompt, instruction="", question="", target_token=200)
         print(compressed_prompt)
+        response_data = compressed_prompt
     
     except Exception as e:
         # Handle exception...
@@ -68,6 +72,24 @@ async def main(request: CompletionRequest):
 
     return response_data
 
+@app.post('/v1/compresslong')
+async def main(request: CompletionRequestLong):
+
+    response_data = None
+    try:
+        print(request.prompt)
+        print(request.question)
+        
+        compressed_prompt = llm_lingua.compress_prompt(request.prompt, instruction="", question=request.question, rank_method="longllmlingua",reorder_context="sort", dynamic_context_compression_ratio=0.2, condition_compare=True, context_budget="+100", token_budget_ratio=1.05)
+        print(compressed_prompt)
+        response_data = compressed_prompt
+    
+    except Exception as e:
+        # Handle exception...
+        logging.error(f"An error occurred: {e}")
+        return {"error": str(e)}
+
+    return response_data
 
 @app.get('/ping')
 async def get_status():
